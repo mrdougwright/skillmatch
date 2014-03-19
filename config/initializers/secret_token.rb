4f52ef6f9d9c1175802acd084cfec403d61cdae8
Doug Wright <mrdougwright@gmail.com>
2014-03-18 22:04:45 -0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skillmatch::Application.config.secret_key_base = '760dc01925e400ded432fbd1bbe80d56a28b8489b1ec98914eaba637764ba4499fbc950ff4589a79808840c867cf1819327bba521d6758338afba36a3d89cb7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skillmatch::Application.config.session_store :cookie_store, key: '_skillmatch_session'
